d67451445196ccbc3d89e1dd0aec906d5b9f5e9b
giuseppebertolini <giuseppe.bertolini@mail.polimi.it>
2018-04-24 16:22:44 +0200

    add fixed AppTest

---
+++ src/test/java/it/polimi/ingsw/AppTest.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+    @Test
+    void myFirstTest() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
+
