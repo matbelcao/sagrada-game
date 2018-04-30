b87a146958d5e3e46e7959e3543e6595054d2de7
matbelcao <10503336@polimi.it>
2018-04-30 18:16:32 +0200

    Create class SchemaCard

---
+++ src/main/java/it/polimi/ingsw/SchemaCard.java
@@ -0,0 +1,15 @@
+package it.polimi.ingsw;
+
+public class SchemaCard extends Card {
+
+    /**
+     * Initialize the common parameters
+     *
+     * @param name   name of the card
+     * @param imgSrc directory path of the image
+     * @param id     unique id of the card
+     */
+    public SchemaCard(String name, String imgSrc, int id) {
+        super(name, imgSrc, id);
+    }
+}
