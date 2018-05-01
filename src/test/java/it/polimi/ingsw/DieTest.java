d5a5fb161d5abffc4f0654381bdcf1169c52b5b1
liviobi <10546479@polimi.it>
2018-05-01 19:31:03 +0200

    added dieTest

---
+++ src/test/java/it/polimi/ingsw/DieTest.java
@@ -0,0 +1,86 @@
+package it.polimi.ingsw;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class DieTest {
+    private static Die test;
+
+    @BeforeEach
+    void beforeAllTests() {
+        test = new Die("ONE", "RED");
+    }
+
+    @Test
+    void init() {
+        assertEquals("ONE", test.getShade());
+        assertEquals("RED", test.getColor());
+        assertEquals("\u001B[31m\u2680\u001B[0m", test.toString());
+    }
+
+    @Test
+    void testIncreaseDecrease() throws IllegalShadeException {
+        test.increaseShade();
+        assertEquals("TWO", test.getShade());
+        assertEquals("RED", test.getColor());
+        test.decreaseShade();
+        assertEquals("ONE", test.getShade());
+        assertEquals("RED", test.getColor());
+    }
+
+    @Test
+    void testSetShade() throws IllegalShadeException {
+        test.setShade(6);
+        assertEquals("SIX", test.getShade());
+        assertEquals("RED", test.getColor());
+    }
+
+    @Test
+    void testflip() throws IllegalShadeException {
+        test.flipShade();
+        assertEquals("SIX", test.getShade());
+        assertEquals("RED", test.getColor());
+        test.flipShade();
+        assertEquals("ONE", test.getShade());
+        test.setShade(2);
+        test.flipShade();
+        assertEquals("FIVE", test.getShade());
+        test.flipShade();
+        assertEquals("TWO", test.getShade());
+        test.setShade(3);
+        test.flipShade();
+        assertEquals("FOUR", test.getShade());
+        test.flipShade();
+        assertEquals("THREE", test.getShade());
+    }
+
+    @Test
+    void decreaseException() {
+        assertThrows(IllegalShadeException.class,
+                () -> {
+                    test.decreaseShade();
+                });
+    }
+
+    @Test
+    void increaseException() throws IllegalShadeException {
+        test.setShade(6);
+        assertThrows(IllegalShadeException.class,
+                () -> {
+                    test.increaseShade();
+                });
+
+    }
+    @Test
+    void setException(){
+        assertThrows(IllegalShadeException.class,
+                () -> {
+                    test.setShade(0);
+                });
+
+    }
+
+}
