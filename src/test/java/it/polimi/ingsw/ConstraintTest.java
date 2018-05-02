07935b6895277186aba73a569e16945c8f120ed7
matbelcao <10503336@polimi.it>
2018-05-02 12:35:48 +0200

    Modified some tests

--- src/test/java/it/polimi/ingsw/ConstraintTest.java
+++ src/test/java/it/polimi/ingsw/ConstraintTest.java
@@ -3,6 +3,7 @@ package it.polimi.ingsw;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class ConstraintTest {
     @Test
@@ -12,7 +13,9 @@ public class ConstraintTest {
         assertEquals(test.getShade(),"null");
         assertEquals(test.isColorConstraint(),Boolean.TRUE);
         assertEquals("\u001B[31m\u25a0\u001B[0m",test.toUtf());
+        assertEquals("RED",test.toString());
         }
+
     @Test
     public void  shadeConstraint(){
         Constraint test = new Constraint("ONE");
@@ -20,5 +23,14 @@ public class ConstraintTest {
         assertEquals("ONE",test.getShade());
         assertEquals(Boolean.FALSE,test.isColorConstraint());
         assertEquals("\u001B[0m\u2680",test.toUtf());
+        assertEquals("ONE",test.toString());
     }
+
+    @Test
+    public void testIsEqual(){
+        Constraint test = new Constraint("ONE");
+        assertEquals(Boolean.TRUE,test.isActive());
+        test.setActive(false);
+        assertEquals(Boolean.FALSE,test.isActive());
     }
+}
