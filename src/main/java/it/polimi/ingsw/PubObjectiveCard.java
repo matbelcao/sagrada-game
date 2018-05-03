a8b7d9cf1bf47afcc1abd7e1de24f7b33e8f7bd1
giuseppebertolini <giuseppe.bertolini@mail.polimi.it>
2018-05-03 20:05:29 +0200

    fix public objective constructor, it now correctly instantiates the ScoreCalculator#

--- src/main/java/it/polimi/ingsw/PubObjectiveCard.java
+++ src/main/java/it/polimi/ingsw/PubObjectiveCard.java
@@ -2,12 +2,27 @@ package it.polimi.ingsw;
 
 import it.polimi.ingsw.scorecalculator.ScoreCalculator;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class PubObjectiveCard extends Card{
     private ScoreCalculator scoreCalculator;
 
     public PubObjectiveCard(int id, String xmlSrc){
         super();
         super.xmlReader(id,xmlSrc,"PubObjectiveCard");
+
+        String className = "ScoreCalculator" + id;
+        String fullPathOfTheClass = "it.polimi.ingsw.scorecalculator." + className;
+        Class cls = null;
+        try {
+            cls = Class.forName(fullPathOfTheClass);
+            assert cls != null;
+            this.scoreCalculator = (ScoreCalculator) cls.getDeclaredConstructor().newInstance();
+
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public int getCardScore(SchemaCard schema){
