6ff0d03d437be2dfcb9b79e51339e7360ae2a1ca
giuseppebertolini <giuseppe.bertolini@mail.polimi.it>
2018-05-03 20:20:18 +0200

    add javaDoc to PrivObjectiveCard and PubObjectiveCard

--- src/main/java/it/polimi/ingsw/PubObjectiveCard.java
+++ src/main/java/it/polimi/ingsw/PubObjectiveCard.java
@@ -4,9 +4,17 @@ import it.polimi.ingsw.scorecalculator.ScoreCalculator;
 
 import java.lang.reflect.InvocationTargetException;
 
+/**
+ * This class implements the Cards named "Public Objective" and their score calculating algorithms
+ */
 public class PubObjectiveCard extends Card{
     private ScoreCalculator scoreCalculator;
 
+    /**
+     * Constructs the card setting its id, name, description and score calculating algorithm
+     * @param id the id of the card
+     * @param xmlSrc the address to the xml file containing necessary information to initialize the cards
+     */
     public PubObjectiveCard(int id, String xmlSrc){
         super();
         super.xmlReader(id,xmlSrc,"PubObjectiveCard");
@@ -25,6 +33,11 @@ public class PubObjectiveCard extends Card{
 
     }
 
+    /**
+     * this method calls the card-specific method for calculating the score obtained
+     * @param schema the schema card the score needs to be calculated on
+     * @return the actual score given by the card
+     */
     public int getCardScore(SchemaCard schema){
         return scoreCalculator.calculateScore(schema);
     }
