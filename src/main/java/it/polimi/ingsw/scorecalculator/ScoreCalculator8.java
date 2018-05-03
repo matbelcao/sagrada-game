ee511b0e5d074b023b1c936fa5756545337fcdc3
matbelcao <10503336@polimi.it>
2018-05-03 18:13:07 +0200

    Implamented score method for PubObjCard num 8

--- src/main/java/it/polimi/ingsw/scorecalculator/ScoreCalculator8.java
+++ src/main/java/it/polimi/ingsw/scorecalculator/ScoreCalculator8.java
@@ -1,7 +1,11 @@
 package it.polimi.ingsw.scorecalculator;
 
+import it.polimi.ingsw.FullCellIterator;
 import it.polimi.ingsw.SchemaCard;
 
+/**
+ * "Different Shades" card implementation
+ */
 public class ScoreCalculator8 implements ScoreCalculator{
 
     /**
@@ -11,6 +15,44 @@ public class ScoreCalculator8 implements ScoreCalculator{
      */
     @Override
     public int calculateScore(SchemaCard schema){
-        return 0;
+        int [] num = {0,0,0,0,0,0};
+        int min;
+
+        FullCellIterator diceIterator=(FullCellIterator)schema.iterator();
+
+        while(diceIterator.hasNext()){
+            diceIterator.next();
+            switch (schema.getCell(diceIterator.getRow(),diceIterator.getColumn()).getDie().getShadeInt()){
+                case 1:
+                    num[0]++;
+                    break;
+                case 2:
+                    num[1]++;
+                    break;
+                case 3:
+                    num[2]++;
+                    break;
+                case 4:
+                    num[3]++;
+                    break;
+                case 5:
+                    num[4]++;
+                    break;
+                case 6:
+                    num[5]++;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        min=num[0];
+        for(int  x : num) {
+            if ( x < min){
+                min = x;
+            }
+        }
+
+        return min;
     }
 }
