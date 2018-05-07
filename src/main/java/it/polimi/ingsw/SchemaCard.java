5020eed64da7894ac2b5dc2822b6b1f5deb2dce1
giuseppebertolini <giuseppe.bertolini@mail.polimi.it>
2018-05-07 17:09:07 +0200

    adding the possibility to specify ignored constraints while checking possible placements or while placing a die

--- src/main/java/it/polimi/ingsw/SchemaCard.java
+++ src/main/java/it/polimi/ingsw/SchemaCard.java
@@ -124,10 +124,9 @@ public class SchemaCard implements Iterable<Cell> {
      * @param die the die we want to place
      * @return the list of valid positions for the die
      */
-    public List<Integer> listPossiblePlacements(Die die) throws IllegalDieException{
+    public List<Integer> listPossiblePlacements(Die die) {
         ArrayList <Integer> list= new ArrayList<>();
         FullCellIterator diceIterator;
-        Cell fullCell;
         diceIterator=new FullCellIterator(this.cell);
 
         if(isFirstDie){
@@ -135,16 +134,31 @@ public class SchemaCard implements Iterable<Cell> {
             checkBorder(die, list);
         }else{
             while(diceIterator.hasNext()){
-                fullCell=diceIterator.next();
-                if(die.equals(fullCell.getDie())){
-                    list.clear();
-                    throw new IllegalDieException("! Trying to place the same die twice");
-                }
+                diceIterator.next();
                 checkCloseCells(die, diceIterator, list);
             }
         }
         return list;
     }
+
+    public List<Integer> listPossiblePlacements(Die die,String ignoreConstraint) {
+        ArrayList <Integer> list= new ArrayList<>();
+        FullCellIterator diceIterator;
+        diceIterator=new FullCellIterator(this.cell);
+
+        if(isFirstDie){
+            //first and last rows
+            checkBorder(die, list,ignoreConstraint);
+        }else{
+            while(diceIterator.hasNext()){
+                diceIterator.next();
+
+                checkCloseCells(die, diceIterator, list, ignoreConstraint);
+            }
+        }
+        return list;
+    }
+
 
     /**
      * Checks if cells close to the one given by the iterator(that contains a die) could accept the die
@@ -164,6 +178,33 @@ public class SchemaCard implements Iterable<Cell> {
                     index=(row * NUM_COLS + column);
 
                     if (!list.contains(index) && canBePlacedHere(row, column, die)) {
+                        list.add(index);
+                    }
+
+                }
+            }
+        }
+    }
+
+    /**
+     * Checks if cells close to the one given by the iterator(that contains a die) could accept the die
+     * @param die the die to be possibly put in place
+     * @param diceIterator iterates on full cells
+     * @param list the list of possible positions for the die
+     * @param ignoreConstraint string that tells which constraint(s) can be ignored
+     */
+    private void checkCloseCells(Die die, FullCellIterator diceIterator, ArrayList<Integer> list, String ignoreConstraint) {
+        int row;
+        int column;
+        Integer index;
+        for(int i = -1; i<2; i++){
+            row=diceIterator.getRow()+i;
+            for(int j=-1; j<2; j++){
+                column=diceIterator.getColumn()+j;
+                if( (row>=0 && row<NUM_ROWS) && (column>=0 && column<NUM_COLS)){
+                    index=(row * NUM_COLS + column);
+
+                    if (!list.contains(index) && canBePlacedHere(row, column, die,ignoreConstraint)) {
                         list.add(index);
                     }
 
@@ -201,6 +242,34 @@ public class SchemaCard implements Iterable<Cell> {
     }
 
     /**
+     * Calculates a list of indexes where the die can be put, if it's the first being placed (only on the border of the schema card)
+     * @param die die to be put in place
+     * @param list list of possible placements
+     * @param ignoreConstraint string that tells which constraint(s) can be ignored
+     */
+    private void checkBorder(Die die, ArrayList<Integer> list,String ignoreConstraint) {
+        int row;
+        int column;
+        //first and last rows
+        for(row=0,column=0; column < NUM_COLS; column++){
+            if(this.cell[row][column].canAcceptDie(die,ignoreConstraint)){
+                list.add(row * NUM_COLS + column);
+            }
+            if(this.cell[row+NUM_ROWS-1][column].canAcceptDie(die,ignoreConstraint)){
+                list.add((row+NUM_ROWS-1) * NUM_COLS + column);
+            }
+        }
+        //first and last columns
+        for(row=0,column=0; row<NUM_ROWS-1;row++){
+            if(this.cell[row][column].canAcceptDie(die,ignoreConstraint)){
+                list.add(row * NUM_COLS );
+            }
+            if(this.cell[row][column+NUM_COLS-1].canAcceptDie(die,ignoreConstraint)){
+                list.add(row * NUM_COLS + NUM_COLS-1);
+            }
+        }
+    }
+    /**
      * Checks whether or not a die can be placed in a cell that is known to be adiacent to a die that is already placed
      * @param row  the row of the cell to be checked
      * @param column the column of the cell to be checked
@@ -209,9 +278,9 @@ public class SchemaCard implements Iterable<Cell> {
      */
     @NotNull
     private Boolean canBePlacedHere (int row, int column, Die die){
-        if(!this.cell[row][column].canAcceptDie(die)){ return false; }
-
         if(this.cell[row][column].hasDie()){ return false; }
+
+        if(!this.cell[row][column].canAcceptDie(die)){ return false; }
 
         if((row > 0)                    && this.cell[row - 1][column].hasDie() && this.cell[row - 1][column].checkNeighbor(die)) { return false; }
 
@@ -219,23 +288,60 @@ public class SchemaCard implements Iterable<Cell> {
 
         if((column > 0)                 && this.cell[row][column - 1].hasDie() && this.cell[row][column - 1].checkNeighbor(die)) { return false; }
 
-        if((column < (NUM_COLS - 1))    && this.cell[row][column + 1].hasDie() && this.cell[row][column + 1].checkNeighbor(die)) { return false; }
+        return (column >= (NUM_COLS - 1)) || !this.cell[row][column + 1].hasDie() || !this.cell[row][column + 1].checkNeighbor(die);
+    }
 
-        return true;
+    /**
+     * Checks whether or not a die can be placed in a cell that is known to be adiacent to a die that is already placed
+     * @param row  the row of the cell to be checked
+     * @param column the column of the cell to be checked
+     * @param die the die we want to place
+     * @param ignoreConstraint string that tells which constraint(s) can be ignored
+     * @return true iff the die can be placed there
+     */
+    @NotNull
+    private Boolean canBePlacedHere (int row, int column, Die die,String ignoreConstraint){
+        if(this.cell[row][column].hasDie()){ return false; }
+
+        if(!this.cell[row][column].canAcceptDie(die,ignoreConstraint)){ return false; }
+
+        if((row > 0)                    && this.cell[row - 1][column].hasDie() && this.cell[row - 1][column].checkNeighbor(die)) { return false; }
+
+        if((row < (NUM_ROWS - 1))       && this.cell[row + 1][column].hasDie() && this.cell[row + 1][column].checkNeighbor(die)) { return false; }
+
+        if((column > 0)                 && this.cell[row][column - 1].hasDie() && this.cell[row][column - 1].checkNeighbor(die)) { return false; }
+
+        return (column >= (NUM_COLS - 1)) || !this.cell[row][column + 1].hasDie() || !this.cell[row][column + 1].checkNeighbor(die);
     }
 
     /**
      * Puts the die in place if possible, if not an exception is thrown
      * @param index index (0 to 19) where the die is going to be put
      * @param die die to be put
+     * @throws IllegalDieException iff the die can't be placed there
      */
-    public void putDie (Integer index,Die die)throws IllegalDieException{
+    public void putDie (Integer index,Die die) throws IllegalDieException{
 
-        assert this.listPossiblePlacements(die).contains(index);
+        if (!this.listPossiblePlacements(die).contains(index)) throw new IllegalDieException("You can't place this die here!");
 
         this.cell[index/NUM_COLS][index%NUM_COLS].setDie(die);
         this.isFirstDie=false;
-        }
+    }
+
+    /**
+     * Puts the die in place if possible, if not an exception is thrown
+     * @param index index (0 to 19) where the die is going to be put
+     * @param die die to be put
+     * @param ignoreConstraint string that tells which constraint(s) can be ignored
+     * @throws IllegalDieException iff the die can't be placed there
+     */
+    public void putDie (Integer index,Die die, String ignoreConstraint) throws IllegalDieException {
+
+        if (!this.listPossiblePlacements(die,ignoreConstraint).contains(index)) throw new IllegalDieException("You can't place this die here!");
+
+        this.cell[index/NUM_COLS][index%NUM_COLS].setDie(die,ignoreConstraint);
+        this.isFirstDie=false;
+    }
 
     /**
      * Instantiates an iterator for cells containing a die
