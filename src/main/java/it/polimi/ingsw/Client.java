53ce0f5af08849489489d2d40f29cb5fe00eea3b
liviobi <10546479@polimi.it>
2018-05-10 01:54:37 +0200

    added loginRMI to client

--- src/main/java/it/polimi/ingsw/Client.java
+++ src/main/java/it/polimi/ingsw/Client.java
@@ -1,5 +1,10 @@
 package it.polimi.ingsw;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
 public class Client {
     private boolean usesGUI;
     private boolean usesRMI;
@@ -15,10 +20,33 @@ public class Client {
 
     public String getPassword(){ return password; }
 
-    public void setUsername(String username){
-        this.username = username;
+    public void setUsername(String username){ this.username = username; }
+
+    public void setPassword(String password) { this.password = password; }
+
+    public void setupConnection(){
+        if(usesRMI){
+            loginRMI();
+        }else{
+            loginSocket();
+        }
     }
-    public void setPassword(String password) {
-        this.password = password;
+
+    void loginRMI(){
+        try {
+            //authenticator = (Authentication)Naming.lookup("rmi://localhost:1099/auth");
+            AuthenticationInt autenticator=(AuthenticationInt) Naming.lookup("rmi://127.0.0.1/myabc");
+            autenticator.authenticate(username,password);
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void loginSocket(){
+
     }
 }
