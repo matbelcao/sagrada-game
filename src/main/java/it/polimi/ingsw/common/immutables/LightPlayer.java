7954aa17cc39be7984712a26eb087d749688f84e
liviobi <10546479@polimi.it>
2018-05-29 17:11:36 +0200

    added method to convert from player to light player

--- src/main/java/it/polimi/ingsw/common/immutables/LightPlayer.java
+++ src/main/java/it/polimi/ingsw/common/immutables/LightPlayer.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.common.immutables;
 
+import it.polimi.ingsw.server.model.Player;
+
 public class LightPlayer {
     String username;
     int playerId;
@@ -11,6 +13,15 @@ public class LightPlayer {
         assert (playerId < 4 && playerId >= 0);
         this.playerId = playerId;
         this.points=0;
+    }
+
+    public static LightPlayer toLightPlayer(Player player){
+        String username = player.getUsername();
+        int playerId = player.getGameId();
+        LightPlayer lightPlayer = new LightPlayer(username,playerId);
+        lightPlayer.setPoints(player.getScore());
+        lightPlayer.setSchema(LightSchemaCard.toLightSchema(player.getSchema()));
+        return lightPlayer;
     }
 
     public String getUsername() {
