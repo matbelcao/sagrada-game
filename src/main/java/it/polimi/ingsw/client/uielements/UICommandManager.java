985e9934ed6eb5441b64093d01698989c092bede
giuseppebertolini <giuseppe.bertolini@mail.polimi.it>
2018-06-14 01:57:41 +0200

    small refactoring of UICommandManager

--- src/main/java/it/polimi/ingsw/client/uielements/UICommandManager.java
+++ src/main/java/it/polimi/ingsw/client/uielements/UICommandManager.java
@@ -41,132 +41,14 @@ public class UICommandManager extends Thread {
 
             command = commandQueue.readln();
             commandQueue.pop();
+
             if (command.matches(INDEX)) {
-                int index = Integer.parseInt(command);
-                switch (client.getTurnState()) {
 
-                    case CHOOSE_SCHEMA:
-                        if (client.getClientConn().choose(index)) {
+                manageIndex(command);
 
-                            client.getClientUI().showWaitingForGameStartScreen();
-                        } else {
-                            client.getClientUI().showLastScreen();
-                        }
-                        break;
-
-                    case MAIN:
-                        switch (index) {
-                            case 0:
-                                client.getBoard().setLastDiceList(client.getClientConn().getDiceList());
-
-                                synchronized (client.getLockState()) {
-                                    client.setTurnState(client.getTurnState().nextState(false, false, false, false));
-                                    client.getLockState().notifyAll();
-                                }
-                                break;
-                            case 1:
-                                synchronized (client.getLockState()) {
-                                    client.setTurnState( client.getTurnState().nextState(true, false, false, false));
-                                    client.getLockState().notifyAll();
-                                }
-                                break;
-                            default:
-                                client.getClientUI().showLastScreen();
-                        }
-                        client.getClientUI().updateBoard(client.getBoard());
-                        break;
-
-                    case CHOOSE_TOOL:
-                        if (index < client.getBoard().NUM_TOOLS && index >= 0) {
-                            if (client.getClientConn().enableTool(index)) {
-                                synchronized (client.getLockState()) {
-                                    client.setTurnState(client.getTurnState().nextState(true, false, false, false));
-                                    client.getLockState().notifyAll();
-                                }
-                                client.getBoard().setLastDiceList(client.getClientConn().getDiceList());
-                            } else {
-                                synchronized (client.getLockState()) {
-                                    client.setTurnState( client.getTurnState().nextState(false, false, false, false));
-                                    client.getLockState().notifyAll();
-                                }
-                            }
-                            client.getClientUI().updateBoard(client.getBoard());
-                        } else {
-                            client.getClientUI().showLastScreen();
-                        }
-                        break;
-                    case SELECT_DIE:
-                        if (client.getBoard().getDiceList().size() > index && index >= 0) {
-                            client.getBoard().setOptionsList(client.getClientConn().select(index));
-                            if (client.getBoard().getOptionsList().size() == 1) {
-
-                                client.getClientConn().choose(0);
-                                synchronized (client.getLockState()) {
-                                    client.setTurnState(client.getTurnState().nextState(false, false, false, false));
-                                    client.setTurnState(client.getTurnState().nextState(
-                                            client.getBoard().getOptionsList().get(0).equals(Commands.PLACE_DIE),
-                                            false,
-                                            false,
-                                            false));
-                                    client.getLockState().notifyAll();
-                                }
-                            } else {
-                                client.getClientUI().showOptions(client.getBoard().getOptionsList());
-                            }
-                        } else {
-                            client.getClientUI().showLastScreen();
-                        }
-                        break;
-                    case CHOOSE_PLACEMENT:
-                        break;
-                    case CHOOSE_OPTION:
-                        if (client.getClientConn().choose(index)) {
-                            synchronized (client.getLockState()) {
-                                client.setTurnState( client.getTurnState().nextState(
-                                        client.getBoard().getOptionsList().get(index).equals(Commands.PLACE_DIE),
-                                        false,
-                                        false,
-                                        false));
-                                client.getLockState().notifyAll();
-                            }
-                        } else {
-                            client.getClientUI().showLastScreen();
-                        }
-
-                        break;
-
-                    default:
-                        client.getClientUI().showLastScreen();
-                        break;
-                }
             } else if (command.matches(SINGLE_CHAR)) {
 
-                switch (command) {
-
-                    case QUIT:
-                        client.quit();
-                        break;
-                    case END_TURN:
-                        client.getClientConn().endTurn();
-                        break;
-                    case BACK:
-                        client.getClientConn().exit();
-                        break;
-                    case DISCARD:
-                        if (client.getTurnState().equals(ClientFSMState.CHOOSE_PLACEMENT)) {
-                            client.getClientConn().discard();
-                        }
-                        break;
-
-                    default:
-                        client.getClientUI().showLastScreen();
-                        break;
-                }
-
-                synchronized (client.getLockState()) {
-                    client.setTurnState(client.getTurnState().nextState(false, command.equals(BACK), command.equals(END_TURN), command.equals(DISCARD)));
-                    client.getLockState().notifyAll();
-                }
+                manageOption(command);
 
             } else {
                 client.getClientUI().showLastScreen();
@@ -174,5 +56,159 @@ public class UICommandManager extends Thread {
 
         }
 
+    }
+
+    private void manageOption(String command) {
+        switch (command) {
+
+            case QUIT:
+                client.quit();
+                break;
+            case END_TURN:
+                client.getClientConn().endTurn();
+                break;
+            case BACK:
+                client.getClientConn().exit();
+                break;
+            case DISCARD:
+                if (client.getTurnState().equals(ClientFSMState.CHOOSE_PLACEMENT)) {
+                    client.getClientConn().discard();
+                }
+                break;
+
+            default:
+                client.getClientUI().showLastScreen();
+                break;
+        }
+
+        synchronized (client.getLockState()) {
+            client.setTurnState(client.getTurnState().nextState(false, command.equals(BACK), command.equals(END_TURN), command.equals(DISCARD)));
+            client.getLockState().notifyAll();
+        }
+    }
+
+    private void manageIndex(String command) {
+        int index = Integer.parseInt(command);
+        switch (client.getTurnState()) {
+
+            case CHOOSE_SCHEMA:
+                chooseSchemaAction(index);
+                break;
+
+            case MAIN:
+                mainChoiceAction(index);
+                break;
+
+            case CHOOSE_TOOL:
+                chooseToolAction(index);
+                break;
+            case SELECT_DIE:
+                selectDieAction(index);
+                break;
+            case CHOOSE_PLACEMENT:
+                break;
+            case CHOOSE_OPTION:
+                chooseOptionAction(index);
+                break;
+
+            default:
+                client.getClientUI().showLastScreen();
+                break;
+        }
+    }
+
+    private void chooseOptionAction(int index) {
+        if (client.getClientConn().choose(index)) {
+            synchronized (client.getLockState()) {
+                client.setTurnState( client.getTurnState().nextState(
+                        client.getBoard().getOptionsList().get(index).equals(Commands.PLACE_DIE),
+                        false,
+                        false,
+                        false));
+                client.getLockState().notifyAll();
+            }
+        } else {
+            client.getClientUI().showLastScreen();
+        }
+
+        return;
+    }
+
+    private void selectDieAction(int index) {
+        if (client.getBoard().getDiceList().size() > index && index >= 0) {
+            client.getBoard().setOptionsList(client.getClientConn().select(index));
+            if (client.getBoard().getOptionsList().size() == 1) {
+
+                client.getClientConn().choose(0);
+                synchronized (client.getLockState()) {
+                    client.setTurnState(client.getTurnState().nextState(false, false, false, false));
+                    client.setTurnState(client.getTurnState().nextState(
+                            client.getBoard().getOptionsList().get(0).equals(Commands.PLACE_DIE),
+                            false,
+                            false,
+                            false));
+                    client.getLockState().notifyAll();
+                }
+            } else {
+                client.getClientUI().showOptions(client.getBoard().getOptionsList());
+            }
+        } else {
+            client.getClientUI().showLastScreen();
+        }
+        return;
+    }
+
+    private void chooseToolAction(int index) {
+        if (index < client.getBoard().NUM_TOOLS && index >= 0) {
+            if (client.getClientConn().enableTool(index)) {
+                synchronized (client.getLockState()) {
+                    client.setTurnState(client.getTurnState().nextState(true, false, false, false));
+                    client.getLockState().notifyAll();
+                }
+                client.getBoard().setLastDiceList(client.getClientConn().getDiceList());
+            } else {
+                synchronized (client.getLockState()) {
+                    client.setTurnState( client.getTurnState().nextState(false, false, false, false));
+                    client.getLockState().notifyAll();
+                }
+            }
+            client.getClientUI().updateBoard(client.getBoard());
+        } else {
+            client.getClientUI().showLastScreen();
+        }
+        return;
+    }
+
+    private void mainChoiceAction(int index) {
+        switch (index) {
+            case 0:
+                client.getBoard().setLastDiceList(client.getClientConn().getDiceList());
+
+                synchronized (client.getLockState()) {
+                    client.setTurnState(client.getTurnState().nextState(false, false, false, false));
+                    client.getLockState().notifyAll();
+                }
+                break;
+            case 1:
+                synchronized (client.getLockState()) {
+                    client.setTurnState( client.getTurnState().nextState(true, false, false, false));
+                    client.getLockState().notifyAll();
+                }
+                break;
+            default:
+                client.getClientUI().showLastScreen();
+        }
+        client.getClientUI().updateBoard(client.getBoard());
+        return;
+    }
+
+    private void chooseSchemaAction(int index) {
+        if (client.getClientConn().choose(index)) {
+
+            client.getClientUI().showWaitingForGameStartScreen();
+        } else {
+            client.getClientUI().showLastScreen();
+        }
+        return;
     }
 }
