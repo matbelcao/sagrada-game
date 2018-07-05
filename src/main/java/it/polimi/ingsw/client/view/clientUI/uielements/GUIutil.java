fbca4502af192047fa0ef3b91259bb35c3362d29
liviobi <10546479@polimi.it>
2018-07-05 22:51:55 +0200

    moved turn indicator

--- src/main/java/it/polimi/ingsw/client/view/clientUI/uielements/GUIutil.java
+++ src/main/java/it/polimi/ingsw/client/view/clientUI/uielements/GUIutil.java
@@ -218,12 +218,22 @@ public class GUIutil {
     }
 
 
-    public VBox drawCards(LightCard privObj, List<LightCard> pubObjs, List<LightTool> tools, double cellDim, ClientFSMState turnState) {
+    public VBox drawCards(LightCard privObj, List<LightCard> pubObjs, List<LightTool> tools, double cellDim, ClientFSMState turnState,boolean isFirstTurn) {
         Button priv = new Button("Private Objective");
         Button pub = new Button("Public Objectives");
         Button tool = new Button("Tools");
 
-        HBox buttonContainer = new HBox(priv, pub, tool);
+        Label turnIndicator = new Label();
+        turnIndicator.setId("turn-indicator");
+        if(isFirstTurn) {
+            turnIndicator.setText(uimsg.getMessage(FIRST_TURN));//todo hookup with uimsg
+        }else{
+                turnIndicator.setText(uimsg.getMessage(SECOND_TURN));
+        }
+
+        Region spacer = new Region();
+        HBox buttonContainer = new HBox(priv, pub, tool, spacer, turnIndicator);
+        HBox.setHgrow(spacer,Priority.ALWAYS);
         HBox cardContainer = new HBox();
         VBox primaryContainer = new VBox(buttonContainer, cardContainer);
 
@@ -296,28 +306,20 @@ public class GUIutil {
         return backPane;
     }
 
-    public VBox getPlayersAndInfoPane(int myPlayerId, LightBoard board){
-        Text turnText = new Text();
+    public VBox getPlayersAndInfoPane(int myPlayerId, LightBoard board){ //todo change name
         Text currentlyPlaying = new Text();
         HBox playerSelector = getPlayersStatusBar(myPlayerId, board);
 
         int font = 24; //todo set dynamic
-
-        if(board.getIsFirstTurn()) {
-            turnText.setText("Turno 1");//todo hookup with uimsg
-        }else{
-            turnText.setText("Turno 2");
-        }
 
         if(board.getNowPlaying() == myPlayerId){
             currentlyPlaying.setText("E' il tuo turno");
         }else{
             currentlyPlaying.setText("1234567890123 sta giocando");
         }
-        turnText.setFont(new Font(FONT,font));
         currentlyPlaying.setFont(new Font(FONT,font));
 
-        VBox playerSelectorAndMessage = new VBox(turnText,currentlyPlaying,playerSelector);
+        VBox playerSelectorAndMessage = new VBox(currentlyPlaying,playerSelector);
         playerSelectorAndMessage.setAlignment(Pos.BOTTOM_LEFT);
         return  playerSelectorAndMessage;
     }
