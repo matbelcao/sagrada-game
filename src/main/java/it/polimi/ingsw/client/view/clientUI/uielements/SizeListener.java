c9030d07430e6b6aa668079b95891d8afdaf56cc
liviobi <10546479@polimi.it>
2018-07-08 18:47:00 +0200

    added javadoc to sizeListener

--- src/main/java/it/polimi/ingsw/client/view/clientUI/uielements/SizeListener.java
+++ src/main/java/it/polimi/ingsw/client/view/clientUI/uielements/SizeListener.java
@@ -6,19 +6,26 @@ import javafx.beans.value.ObservableValue;
 
 import java.util.Timer;
 import java.util.TimerTask;
-
+/**
+ * This class represents a listener of the size of the stage.
+ */
 public class SizeListener implements ChangeListener<Number> {
     private static final int DELAY_TIME = 250; // delay that has to pass in order to consider an operation done
     private Timer timer;
     private final Object lockTimer;
     private GUI gui;
-    private boolean enabled = false;
+    private boolean enabled = false; //resizing can be disabled this way in the scenes that are not meat to be resizable
 
+    /**
+     * Conatructor of the class
+     * @param gui a reference to the gui to be used to draw on it
+     */
     public SizeListener(GUI gui){
         this.timer = new Timer();
         this.lockTimer = new Object();
         this.gui = gui;
     }
+    //when the sizes of the stage change a new timertask starts, if it reaches zero it triggers a redraw of the scene
     @Override
     public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
         synchronized (lockTimer) {
@@ -26,7 +33,7 @@ public class SizeListener implements ChangeListener<Number> {
                     if (timer != null) {
                         timer.cancel();
                     }
-                    timer = new Timer();
+                    timer = new Timer(); //to be shure to delete all the accumulated timer tasks
                     TimerTask task; // task to execute after defined delay
                     task = new TimerTask() {// create new task that calls resize operation
                         @Override
@@ -42,6 +49,9 @@ public class SizeListener implements ChangeListener<Number> {
             }
     }
 
+    /**
+     * this method deletes the timer and gets called everytime the size listener gets disabled or the stage gets resized
+     */
     public void purgeTimer(){
         synchronized (lockTimer) {
           if(timer!=null){
@@ -50,10 +60,15 @@ public class SizeListener implements ChangeListener<Number> {
         }
     }
 
+    /**
+     * This method enables the listener
+     */
     public void enable() {
         this.enabled = true;
     }
-
+    /**
+     * This method disables the listener
+     */
     public void disable() {
         purgeTimer();
         this.enabled = false;
