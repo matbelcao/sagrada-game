7e4f48c33d520570dcd116a9add64d62142cd076
liviobi <10546479@polimi.it>
2018-07-08 18:27:41 +0200

    added javadoc to customGUIEvent

--- src/main/java/it/polimi/ingsw/client/view/clientUI/uielements/CustomGuiEvent.java
+++ src/main/java/it/polimi/ingsw/client/view/clientUI/uielements/CustomGuiEvent.java
@@ -3,19 +3,35 @@ package it.polimi.ingsw.client.view.clientUI.uielements;
 import javafx.event.Event;
 import javafx.event.EventType;
 
+/**
+ * This class represent events meant to be fired only in particular occasion in the game by nodes. Those events are specifically handled
+ * by the parents of the nodes that fire those events so that they can switch the view.
+ * During the game the view changes only when the client updates it, except when such events are fired.
+ */
 public class CustomGuiEvent extends Event {
     private int eventObjectIndex;
     public static final EventType<CustomGuiEvent> MOUSE_EXITED_BACK_PANE = new EventType<>("MOUSE_EXITED_BACK_PANE");
     public static final EventType<CustomGuiEvent> MOUSE_ENTERED_MULTIPLE_DICE_CELL = new EventType<>("MOUSE_ENTERED_MULTIPLE_DICE_CELL");
     public static final EventType<CustomGuiEvent> SELECTED_PLAYER = new EventType<>("SELECTED_PLAYER");
 
+    /**
+     * Constructor of the class
+     * @param eventType he event type of the custom gui class
+     * @param eventObjectIndex the index of the node firing the event in its container
+     */
     CustomGuiEvent(EventType<? extends Event> eventType, int eventObjectIndex) {
         super(eventType);
         this.eventObjectIndex = eventObjectIndex;
     }
+
+    /**
+     * Constructor of the class
+     * @param eventType the event type of the custom gui class
+     */
     CustomGuiEvent(EventType<? extends Event> eventType) {
         super(eventType);
     }
+
     public int getEventObjectIndex(){
         return eventObjectIndex;
     }
